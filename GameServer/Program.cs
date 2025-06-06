c2897366e5b8750805d646d18e1b6ef3a5899ef7
Mateusz Stanuch <matitonie@o2.pl>
2025-06-06 19:28:08 +0200

    forgot port

--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
             "http://localhost:5224",
             "http://localhost",
             "http://192.168.1.46:5173",
-            "https://maszrum.crabdance.com"
+            "https://maszrum.crabdance.com:5173"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
