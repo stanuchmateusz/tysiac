1de6dd45bce9bc7d1b952ad2c2dbc89cb9f3983b
Mateusz Stanuch <matitonie@o2.pl>
2025-06-18 19:42:26 +0200

    licence

--- tysiac-front/src/components/Footer.tsx
+++ tysiac-front/src/components/Footer.tsx
@@ -6,8 +6,7 @@ const Footer = () => {
                     &copy; {new Date().getFullYear()} Tysiac siadany.
                 </p>
                 <p className="text-xs mt-2">
-                    This project is licensed under the MIT License.
-
+                    This project is licensed under the Creative Commons Attribution-NonCommercial 4.0 International (CC-BY-NC-4.0) License.
                 </p>
             </div>
         </footer>
